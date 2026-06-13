76472a890a9ac730c46ae23bbda4a362a50e4aae
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2026-06-13 19:23:34 +0200

    Moved config to separate json file

--- CheckNewVersion.ps1
+++ CheckNewVersion.ps1
@@ -1,12 +1,28 @@
-$outDirectory = ".\latest\"
-$tempDirectory = ".\temp\"
 $tempFileName = "tmp.zip"
-$repo = "username/reponame"
-$fileToDate = "something.exe"
-$assetNameRegex = "^Windows"
+$tempDirectory = ".\temp\"
 
-$includeDrafts = $false
-$includePrerelease = $false
+# READ CONFIG
+#region Config
+# Find the config file
+$ConfigJsonFilename = ""
+if (($args.Count -gt 0) -and (Test-Path $args[0])) {
+  $ConfigJsonFilename = $args[0]
+}
+elseif ((Get-ChildItem *.json).Length -gt 0) {
+  $ConfigJsonFilename = Get-ChildItem *.json | Select-Object -First 1
+}
+else {
+  Write-Host "No config file found!"
+  Exit
+}
+
+# Get the contents as JSON
+$JSON = Get-Content $ConfigJsonFilename | ConvertFrom-Json
+
+#endregion
+
+# HELPER FUNCTIONS
+#region Helper functions
 
 function Write-TwoCols {
   param (
@@ -21,8 +37,12 @@ function Write-TwoCols {
   $paddingAmount = $lengths.Maximum - $leftColText.Length
   
   Write-Host $leftColText $rightColText.PadLeft($paddingAmount) -ForegroundColor $color
-
 }
+
+#endregion
+
+# UPGRADE FUNCTION
+#region Upgrade function
 
 function Perform-Upgrade {
   param (
@@ -52,7 +72,7 @@ function Perform-Upgrade {
 
   # Copy/Overwrite what is in the emulator folder
   Write-Host " Copying..." -ForegroundColor Magenta
-  Copy-item -Force -Recurse $expandedFilesDir -Destination $outDirectory
+  Copy-item -Force -Recurse $expandedFilesDir -Destination $JSON.out_directory
 
   # Delete downloaded temp files when done to prep for next version
   Write-Host " Cleaning up..." -ForegroundColor Magenta
@@ -61,22 +81,39 @@ function Perform-Upgrade {
   Write-Host " DONE" -ForegroundColor Magenta
 }
 
-# Get latest release info
-$jsonResponse = Invoke-RestMethod -Uri ("https://api.github.com/repos/" + $repo + "/releases")
+#endregion
 
-# Find first valid response
+
+
+# MAIN SCRIPT
+#region Main script
+
+Write-host ("Looking for updates to " + $JSON.name)
+
+
+# # Get latest release info
+$jsonResponse = Invoke-RestMethod -Uri ("https://api.github.com/repos/" + $JSON.repo + "/releases")
+
+# Find first valid release & asset
 $validRelease = ($jsonResponse | Where-Object { 
-  ($_.draft -eq $false -or $includeDrafts -eq $true) -and
-  ($_.prerelease -eq $false -or $includePrerelease -eq $true)
-} | Select-Object -First 1)
+    ($_.draft -eq $false -or $JSON.include_drafts -eq $true) -and
+    ($_.prerelease -eq $false -or $JSON.include_prerelease -eq $true)
+  } | Select-Object -First 1)
 
 $validAsset = ($validRelease.assets | Where-Object {
-  $_.name -match $assetNameRegex
-} | Select-Object -First 1)
+    $_.name -match $JSON.asset_name_regex
+  } | Select-Object -First 1)
 
+$validAsset.name
 
 # Get dates
-$exeLastUpdatedDate = if (Test-Path (Join-Path $outDirectory $fileToDate)) {(Get-Item (Join-Path $outDirectory $fileToDate)).LastWriteTime} else {Get-Date -Date "1970-01-01 00:00:00Z"}
+$fileToDateFullpath = Join-Path $JSON.out_directory $JSON.file_to_date
+$exeLastUpdatedDate = if (Test-Path $fileToDateFullpath) { 
+  (Get-Item $fileToDateFullpath).LastWriteTime 
+}
+else { 
+  Get-Date -Date "1970-01-01 00:00:00Z" 
+}
 $lastReleaseDate = [datetime]$validAsset.updated_at
 
 # Compare dates
@@ -86,10 +123,12 @@ Write-TwoCols " Latest release:" $lastReleaseDate.Date.ToString("yyyyMMdd") 35 G
 if ($lastReleaseDate.Date -gt $exeLastUpdatedDate.Date) {
   Write-Host " New version detected!"
 
-  Perform-Upgrade $validAsset $tempDirectory $outDirectory
+  Perform-Upgrade $validAsset $tempDirectory $JSON.out_directory
 }
 else {
   Write-Host "No new version detected"
 }
 
-# Read-Host -Prompt "Press Enter to exit"
+Read-Host -Prompt "Press Enter to exit"
+
+#endregion
